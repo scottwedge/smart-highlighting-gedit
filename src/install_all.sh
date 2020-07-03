3ea73ad8f29897283f3c414c67d9dff631aa8dd7
swedge <scott.wedge@gmail.com>
2020-07-03 11:19:38 -0400

    Fixes #20

--- src/install_all.sh
+++ src/install_all.sh
@@ -9,10 +9,10 @@ PLUGIN_DEST=/usr/lib/gedit-2/plugins/
 # create it
 sudo mkdir -p ${PLUGIN_DEST}
 
-# remove previous verision and currect version of plugin
+# remove previous version and current version of plugin
 sudo rm -rf ${PLUGIN_DEST}/${PLUGIN_NAME}*
 
-# install current verion of plugin
+# install current version of plugin
 sudo cp -rv ${PLUGIN_NAME}* ${PLUGIN_DEST}
 
 LOCALE_DEST=/usr/share/locale
--- src/remove.sh
+++ src/remove.sh
@@ -9,10 +9,10 @@ PLUGIN_DEST=~/.gnome2/gedit/plugins/
 # create it
 #mkdir -p ${PLUGIN_DEST}
 
-# remove previous verision and currect version of plugin
+# remove previous version and current version of plugin
 rm -rf ${PLUGIN_DEST}/${PLUGIN_NAME}*
 
-# install current verion of plugin
+# install current version of plugin
 #cp -rv ${PLUGIN_NAME}* ${PLUGIN_DEST}
 
 LOCALE_DEST=/usr/share/locale
